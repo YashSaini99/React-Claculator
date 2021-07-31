efb1dcbd9760f35fadae2c563d96caf1b37de464
YasH⚡ <74340276+YashSaini99@users.noreply.github.com>
2021-07-31 08:48:42 +0530

    Update history.js

--- src/components/history.js
+++ src/components/history.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-// This is a functional component which shows the users calculated history
+
 const History = (props) => {
     const historyList = props.history.map((obj, index) => {
         return (<tr key={index} ><td onClick={()=> props.updateDisplayHistory(obj.exp)}><span className="exp">{obj.exp} = </span>{obj.result}</td></tr>) 
